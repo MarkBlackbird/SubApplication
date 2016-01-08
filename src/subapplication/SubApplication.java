88487aa19cf7e1f92e7604786f97b88254d37583
MarkBlackbird <kos.marek92@gmail.com>
2016-01-08 13:38:28 +0100

    Moze jakies zmiany
    Nie wiem czy cos tu modyfikowalem, pewnie nie

--- src/subapplication/SubApplication.java
+++ src/subapplication/SubApplication.java
@@ -5,6 +5,9 @@
  */
 package subapplication;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 //<<<<<<< HEAD
 //=======
@@ -30,16 +33,20 @@ public class SubApplication {
      */
     public static void main(String[] args) {
 //<<<<<<< HEAD
-        List<String> filecontent = null;
-        try{
-            filecontent = Files.readAllLines(FileSystems.getDefault().getPath("resolv.txt"));
-        } catch (IOException ex){
-            Logger.getLogger(NetworkHandler.class.getName()).log(Level.SEVERE, null, ex);
+        // FileReader reads text files in the default encoding.
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader("resolv.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String netinfo[] = bufferedReader.readLine().split("\\s+");
+            NetworkHandler nh = new NetworkHandler(netinfo[0], Integer.parseInt(netinfo[1]));
+            AlarmGenerator gen = new AlarmGenerator(nh);
+            gen.createAndShowGUI();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SubApplication.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SubApplication.class.getName()).log(Level.SEVERE, null, ex);
         }
-        String netinfo[] = filecontent.get(0).split("\\s+");
-        NetworkHandler nh = new NetworkHandler(netinfo[0], Integer.parseInt(netinfo[1]));
-        AlarmGenerator gen = new AlarmGenerator(nh);
-        gen.createAndShowGUI();
 //=======
         /*NetworkHandler nh = new NetworkHandler("127.0.0.1",10800);
         try {
